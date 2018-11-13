16ddbd6e393e03217f621ffe2a1e26b1b4bb7547
Silverius <silveriuz16@gmail.com>
2018-11-13 13:51:41 +0700

    new d file

---
+++ tcx-ng.d.ts
@@ -0,0 +1,10 @@
+export declare class TcxNg {
+    getAppID():string;
+    getAppPass(o:object):string;
+    getToken(o:object):string;
+    getRefreshToken(o:object):string;
+    clearToken();
+    getMasterToken():string;
+    getTime():string;
+    appendTime(o:object):object;
+}
---
+++ tcx-ng.js
@@ -0,0 +1,9 @@
+import {Injectable} from '@angular/core';
+
+var TcxNg = require('verzth-tcx-js');
+
+export {TcxNg};
+
+TcxNg.decorators = [
+    {type: Injectable}
+];
