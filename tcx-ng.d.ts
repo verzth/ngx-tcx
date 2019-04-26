d8d0486bc32b1f5a5d3aac38621b289b82281548
Silverius <silveriuz16@gmail.com>
2019-04-26 16:34:10 +0700

    Change to TS from JS, add Observable to handle async inside interceptor

--- tcx-ng.d.ts
+++ tcx-ng.d.ts
@@ -1,12 +1,22 @@
+import {Observable} from "rxjs";
+
 export declare class TCX {
     constructor(o:any);
+    static random(): string;
+    static getTime(): string;
     init(o:any);
+    getCookie():object;
+    setCookie(o:object);
     getAppID():string;
     getAppPass(o:object):string;
-    getToken(o:object):string;
-    getRefreshToken(o:object):string;
+    getToken(): Observable<any>;
+    getRefreshToken(): Observable<any>;
     clearToken();
     getMasterToken():string;
     getTime():string;
     appendTime(o:object):object;
+}
+
+export declare interface TCXOption {
+    
 }
